5e2f82fedaf74477aeb17a36d04253eb1648c87b
Lukman Hakim A <lukmannhakimx@gmail.com>
2023-08-25 09:20:24 +0700

    laravel ui

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,22 @@
 |
 */
 
+// Main
 Route::get('/', function () {
     return view('main');
 });
 
+
+// Auth
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login', 'user');
+Route::post('/login', 'Auth\LoginController@login');
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+
+// Admin Dashboard
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

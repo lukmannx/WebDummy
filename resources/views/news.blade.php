f64591ee4997bf01107b34379a7c8968405496f3
Muhammad Faiq Al Harits <88996846+faiqalharits@users.noreply.github.com>
2023-09-01 10:32:54 +0700

    dsad

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -7,179 +7,104 @@
     </div>
 
     <div class="content container-fluid px-7">
-       
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 gy-4 mt-4">
-            
-            <div class="col p-2 ">
-                <a class="card border-1 rounded-0 shadow" href="">
-                    <img src="{{URL::asset('/img/about.jpg')}}" alt="..." class="card-img rounded-0">
-                    <div class="card-body py-2 px-3">
-                        <h5 class="card-title mt-2">
-                            Lorem ipsum dolor sit amet.
-                        </h5>
-                        <div class="card-text pt-1">
-                            <p style="font-size: 15px;" class="text-muted">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae.
-                            </p>
-                            <div class="d-flex justify-content-between pt-2">
-                                <p>
-                                    10.20.30
-                                </p>
-                                <p>
-                                    Lukman
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col p-2">
-                <a class="card border-1 rounded-0 shadow" href="">
-                    <img src="{{URL::asset('/img/about.jpg')}}" alt="..." class="card-img rounded-0">
-                    <div class="card-body py-2 px-3">
-                        <h5 class="card-title mt-2">
-                            Lorem ipsum dolor sit amet.
-                        </h5>
-                        <div class="card-text pt-1">
-                            <p style="font-size: 15px;" class="text-muted">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae.
-                            </p>
-                            <div class="d-flex justify-content-between pt-2">
-                                <p>
-                                    10.20.30
-                                </p>
-                                <p>
-                                    Lukman
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            
-            <div class="col p-2">
-                <a class="card border-1 rounded-0 shadow" href="">
-                    <img src="{{URL::asset('/img/about.jpg')}}" alt="..." class="card-img rounded-0">
-                    <div class="card-body py-2 px-3">
-                        <h5 class="card-title mt-2">
-                            Lorem ipsum dolor sit amet.
-                        </h5>
-                        <div class="card-text pt-1">
-                            <p style="font-size: 15px;" class="text-muted">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae.
-                            </p>
-                            <div class="d-flex justify-content-between pt-2">
-                                <p>
-                                    10.20.30
-                                </p>
-                                <p>
-                                    Lukman
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            
-            <div class="col p-2">
-                <a class="card border-1 rounded-0 shadow" href="">
-                    <img src="{{URL::asset('/img/about.jpg')}}" alt="..." class="card-img rounded-0">
-                    <div class="card-body py-2 px-3">
-                        <h5 class="card-title mt-2">
-                            Lorem ipsum dolor sit amet.
-                        </h5>
-                        <div class="card-text pt-1">
-                            <p style="font-size: 15px;" class="text-muted">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae.
-                            </p>
-                            <div class="d-flex justify-content-between pt-2">
-                                <p>
-                                    10.20.30
-                                </p>
-                                <p>
-                                    Lukman
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            
-            <div class="col p-2">
-                <a class="card border-1 rounded-0 shadow" href="">
-                    <img src="{{URL::asset('/img/about.jpg')}}" alt="..." class="card-img rounded-0">
-                    <div class="card-body py-2 px-3">
-                        <h5 class="card-title mt-2">
-                            Lorem ipsum dolor sit amet.
-                        </h5>
-                        <div class="card-text pt-1">
-                            <p style="font-size: 15px;" class="text-muted">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae.
-                            </p>
-                            <div class="d-flex justify-content-between pt-2">
-                                <p>
-                                    10.20.30
-                                </p>
-                                <p>
-                                    Lukman
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            
-            <div class="col p-2">
-                <a class="card border-1 rounded-0 shadow" href="">
-                    <img src="{{URL::asset('/img/about.jpg')}}" alt="..." class="card-img rounded-0">
-                    <div class="card-body py-2 px-3">
-                        <h5 class="card-title mt-2">
-                            Lorem ipsum dolor sit amet.
-                        </h5>
-                        <div class="card-text pt-1">
-                            <p style="font-size: 15px;" class="text-muted">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae.
-                            </p>
-                            <div class="d-flex justify-content-between pt-2">
-                                <p>
-                                    10.20.30
-                                </p>
-                                <p>
-                                    Lukman
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            
-            <div class="col p-2">
-                <a class="card border-1 rounded-0 shadow" href="">
-                    <img src="{{URL::asset('/img/about.jpg')}}" alt="..." class="card-img rounded-0">
-                    <div class="card-body py-2 px-3">
-                        <h5 class="card-title mt-2">
-                            Lorem ipsum dolor sit amet.
-                        </h5>
-                        <div class="card-text pt-1">
-                            <p style="font-size: 15px;" class="text-muted">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae.
-                            </p>
-                            <div class="d-flex justify-content-between pt-2">
-                                <p>
-                                    10.20.30
-                                </p>
-                                <p>
-                                    Lukman
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            
 
-            
-            
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 gy-4 mt-4">
+            <div class="col p-2 ">
+                <a class="card border-1 rounded-0 shadow member-img" href="">
+                    <img src="{{URL::asset('/img/portfolio/portfolio-2.jpg')}}" alt="..." class= rounded-0" style="display: block; width: 450px; height: 350px; object-fit: cover;">
+                    <div class="card-body py-2 px-3">
+                        <h5 class="card-title mt-2">
+                            Lorem ipsum dolor sit amet.
+                        </h5>
+                        <div class="card-text pt-1">
+                            <p style="font-size: 15px;" class="text-muted">
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae.
+                            </p>
+                            <div class="d-flex justify-content-between pt-2">
+                                <p>
+                                    10.20.30
+                                </p>
+                                <p>
+                                    Lukman
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col p-2 ">
+                <a class="card border-1 rounded-0 shadow member-img" href="">
+                    <img src="{{URL::asset('/img/portfolio/portfolio-2.jpg')}}" alt="..." class= rounded-0" style="display: block; width: 450px; height: 350px; object-fit: cover;">
+                    <div class="card-body py-2 px-3">
+                        <h5 class="card-title mt-2">
+                            Lorem ipsum dolor sit amet.
+                        </h5>
+                        <div class="card-text pt-1">
+                            <p style="font-size: 15px;" class="text-muted">
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae.
+                            </p>
+                            <div class="d-flex justify-content-between pt-2">
+                                <p>
+                                    10.20.30
+                                </p>
+                                <p>
+                                    Lukman
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col p-2 ">
+                <a class="card border-1 rounded-0 shadow member-img" href="">
+                    <img src="{{URL::asset('/img/portfolio/portfolio-3.jpg')}}" alt="..." class= rounded-0" style="display: block; width: 450px; height: 350px; object-fit: cover;">
+                    <div class="card-body py-2 px-3">
+                        <h5 class="card-title mt-2">
+                            Lorem ipsum dolor sit amet.
+                        </h5>
+                        <div class="card-text pt-1">
+                            <p style="font-size: 15px;" class="text-muted">
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae.
+                            </p>
+                            <div class="d-flex justify-content-between pt-2">
+                                <p>
+                                    10.20.30
+                                </p>
+                                <p>
+                                    Lukman
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col p-2 ">
+                <a class="card border-1 rounded-0 shadow member-img" href="">
+                    <img src="{{URL::asset('/img/portfolio/portfolio-1.jpg')}}" alt="..." class= rounded-0" style="display: block; width: 450px; height: 350px; object-fit: cover;">
+                    <div class="card-body py-2 px-3">
+                        <h5 class="card-title mt-2">
+                            Lorem ipsum dolor sit amet.
+                        </h5>
+                        <div class="card-text pt-1">
+                            <p style="font-size: 15px;" class="text-muted">
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae.
+                            </p>
+                            <div class="d-flex justify-content-between pt-2">
+                                <p>
+                                    10.20.30
+                                </p>
+                                <p>
+                                    Lukman
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+
+
+
         </div>
 
     </div>
